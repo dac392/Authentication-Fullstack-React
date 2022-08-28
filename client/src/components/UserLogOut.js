2abca7f33c0dc65840592fdcf717aa1fc3f54c04
dac392 <dac392@scarletmail.rutgers.edu>
2022-08-28 17:25:50 -0400

    add redirect

--- client/src/components/UserLogOut.js
+++ client/src/components/UserLogOut.js
@@ -1,6 +1,5 @@
 import React, { useEffect } from 'react';
 import { useNavigate } from 'react-router-dom';
-// import { Redirect } from 'react-router-dom';
 
 export default () => {
   const navigate = useNavigate();
