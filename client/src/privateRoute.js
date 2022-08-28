9b5a10d6a5324e148d344e3899b0c10199663d21
dac392 <dac392@scarletmail.rutgers.edu>
2022-08-28 13:01:46 -0400

    base files

---
+++ client/src/privateRoute.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import { Route, Redirect } from 'react-router-dom';
+import { Consumer } from './Context';
+
+export default ({ component: Component, ...rest }) => {
+  return (
+    <Consumer>
+      { context => (
+        <Route
+          {...rest}
+        />
+      )}
+    </Consumer>
+  );
+};
