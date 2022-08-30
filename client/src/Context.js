309989372a017a8794da36739c538c9733454497
dac392 <dac392@scarletmail.rutgers.edu>
2022-08-29 20:02:08 -0400

    user information is kept in a cookie for qol

--- client/src/Context.js
+++ client/src/Context.js
@@ -1,21 +1,30 @@
 import React, { useState } from 'react';
+import Cookies from 'js-cookie';
 import Data from './Data';
 
 export const Context = React.createContext(); 
 
 export const Provider = (props) => {
   const data = new Data();
-  const [authUser, setAuthUser] = useState(null);
+  const cookie_data = Cookies.get('authUser');
+  const user_cookie = cookie_data? JSON.parse( cookie_data ) : null;
+  const [authUser, setAuthUser] = useState(user_cookie? user_cookie:null);
 
   const logIn = async (username, password) => {
     const user = await data.getUser(username, password);
     if (user !== null){
       setAuthUser(user);
+
+      // set cookie
+      Cookies.set('authUser', JSON.stringify(user), { expires: 1 });
+      // [ cookie name, cookie value, cookie options ]
+      // cookie option: { expires: 1 } sets the cookie to expire in 1 day
     }
     return user;
   };
   const logOut = () => {
     setAuthUser(null);
+    Cookies.remove('authUser');
   };
   const signup = () => {};
 
